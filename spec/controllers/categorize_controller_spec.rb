c24ecaeb1301258811fce2ba5d56da245768de09
Stephane Bisson <stephane.c.bisson@gmail.com>
2014-02-05 13:48:38 -0500

    remove unwanted tests

--- spec/controllers/categorize_controller_spec.rb
+++ spec/controllers/categorize_controller_spec.rb
@@ -2,25 +2,4 @@ require 'spec_helper'
 
 describe CategorizeController do
 
-  describe "GET 'next'" do
-    it "returns http success" do
-      get 'next'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'choose'" do
-    it "returns http success" do
-      get 'choose'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'wrong'" do
-    it "returns http success" do
-      get 'wrong'
-      response.should be_success
-    end
-  end
-
 end
